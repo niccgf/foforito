949087f98ffd7c01d9a4ab57c381c2cde848af78
Nic <nicolasgfalcon@gmail.com>
2024-01-13 21:52:09 -0300

    Front page terminada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
 $(document).ready(function() {
-    // $("#header").css('display', 'none');
-    // $("#main").css('display', 'none');
-    isDocumentReady = true;
+    var isDocumentReady = true;
+
+    function start() {
+        $("#welcome")
+            .css("display", "flex")
+            .hide()
+            .fadeIn(4000);
+        $(".sub-title")
+            .css("display", "flex")
+            .hide()
+            .fadeIn(5500);
+        $("#inicio").delay(2000)
+            .css("display", "block")
+            .hide()
+            .fadeIn(4000);
+        $("#boton-inicio").delay(4000)
+            .css("display", "block")
+            .hide()
+            .fadeIn(1000);
+    }
+
+    // Ejecutar la función start al cargar la página
+    start();
+
+    // Manejar el evento click en cualquier parte del documento
+    $(document).on("click", function() {
+        if (isDocumentReady) {
+            // Si la función start está en curso, detenerla y mostrar todo con fadeIn rápido
+            $("#welcome, .sub-title, #inicio, #boton-inicio").stop(true, true).fadeIn(100);
+        }
+    });
 });
+
+$("#boton-inicio").click(function(){
+    $("#boton-inicio")
+        .fadeOut(1000)
+        .css("display","hidden")
+    $("#inicio").delay(1000)
+        .fadeOut(1000)
+        .css("display","hidden")
+    $(".sub-title").delay(1200)
+        .fadeOut(1000)
+        .css("display","hidden")
+})
+
 
 $("#play").click(function() {
     $("#welcome").fadeOut("slow",function(){
